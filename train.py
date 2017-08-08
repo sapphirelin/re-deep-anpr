d2d440e92504d8176de1bdf9724cbe41dd266e17
Sapphire Lin <30453370+sapphirelin@users.noreply.github.com>
2017-08-08 11:19:23 +0800

    debugged

--- train.py
+++ train.py
@@ -23,7 +23,6 @@
 """
 重複訓練網路模型
 Routines for training the network.
-
 """
 
 __all__ = (
@@ -34,8 +33,8 @@ import functools
 import glob
 import itertools
 import multiprocessing
-import time
 import os
+import time
 
 import cv2
 import numpy
@@ -51,16 +50,20 @@ def code_to_vec(p, code):
         y = numpy.zeros((len(common.CHARS),))
         y[common.CHARS.index(c)] = 1.0
         return y
+
     c = numpy.vstack([char_to_vec(c) for c in code])
+
     return numpy.concatenate([[1. if p else 0], c.flatten()])
+
 
 def read_data(img_glob):
     for fname in sorted(glob.glob(img_glob)):
         im = cv2.imread(fname)[:, :, 0].astype(numpy.float32) / 255.
-        print("read_data fname:"+fname)
+        print("read_data:"+fname)
         code = fname.split("\\")[1][9:16]
         p = fname.split("\\")[1][17] == '1'
         yield im, code_to_vec(p, code)
+
 
 def unzip(b):
     xs, ys = zip(*b)
@@ -68,6 +71,7 @@ def unzip(b):
     ys = numpy.array(ys)
     return xs, ys
     
+
 def batch(it, batch_size):
     out = []
     for x in it:
@@ -78,8 +82,8 @@ def batch(it, batch_size):
     if out:
         yield out
 
+
 def mpgen(f):
-    print("do multiprocessing")
     def main(q, args, kwargs):
         try:
             for item in f(*args, **kwargs):
@@ -88,13 +92,8 @@ def mpgen(f):
             q.close()
 
     @functools.wraps(f)
-    
     def wrapped(*args, **kwargs):
-        
-        #print("do wrapped ing")
         q = multiprocessing.Queue(3) 
-       
-       #print("main :",main(q, args, kwargs),"q, args, kwargs : ",q, args, kwargs)
         proc = multiprocessing.Process(target=main,
                                        args=(q, args, kwargs))
         proc.start()
@@ -110,9 +109,7 @@ def mpgen(f):
 
 #@mpgen
 def read_batches(batch_size):
-
     g = gen.generate_ims()
-
     def gen_vecs():
         for im, c, p in itertools.islice(g, batch_size):
             yield im, code_to_vec(p, c)
@@ -120,14 +117,14 @@ def read_batches(batch_size):
     while True:
         yield unzip(gen_vecs())
 
+
 def get_loss(y, y_):
-    
     # Calculate the loss from digits being incorrect.  Don't count loss from
     # digits that are in non-present plates.
     digits_loss = tf.nn.softmax_cross_entropy_with_logits(
-                                          labels=tf.reshape(y[:, 1:],
+                                          logits=tf.reshape(y[:, 1:],
                                                      [-1, len(common.CHARS)]),
-                                          logits=tf.reshape(y_[:, 1:],
+                                          labels=tf.reshape(y_[:, 1:],
                                                      [-1, len(common.CHARS)]))
     digits_loss = tf.reshape(digits_loss, [-1, 7])
     digits_loss = tf.reduce_sum(digits_loss, 1)
@@ -136,10 +133,11 @@ def get_loss(y, y_):
 
     # Calculate the loss from presence indicator being wrong.
     presence_loss = tf.nn.sigmoid_cross_entropy_with_logits(
-                                                          labels=y[:, :1], logits=y_[:, :1])
+                                                          labels=y_[:, :1], logits=y[:, :1])
     presence_loss = 7 * tf.reduce_sum(presence_loss)
 
     return digits_loss, presence_loss, digits_loss + presence_loss
+
 
 def train(learn_rate, report_steps, batch_size, initial_weights=None):
     """
@@ -198,38 +196,30 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
                             numpy.all(r[0] == r[1], axis=1),
                             numpy.logical_and(r[2] < 0.5,
                                               r[3] < 0.5)))
-        
-        show_size = 190  ## show_size original = 190
-        r_short = (r[0][:show_size], r[1][:show_size], r[2][:show_size], r[3][:show_size])
+        r_short = (r[0][:batch_size], r[1][:batch_size], r[2][:batch_size], r[3][:batch_size])
         for b, c, pb, pc in zip(*r_short):
             print("{} {} <-> {} {}".format(vec_to_plate(c), pc,
                                            vec_to_plate(b), float(pb)))
         num_p_correct = numpy.sum(r[2] == r[3])
 
-        print("Batch {:3d} 數字準確率: {:2.02f}% 車牌出現率: {:02.02f}% loss: {} "
-               "(digits: {}, presence: {})".format(
-            batch_idx,
-            100. * num_correct / (len(r[0])),
-            100. * num_p_correct / len(r[2]),
-            r[6],
-            r[4],
-            r[5]))
+        print("batch {:3d} correct: {:2.02f}% presence: {:02.02f}% ".format(
+            batch_idx, 100. * num_correct / (len(r[0])), 100. * num_p_correct / len(r[2])))
+        print("loss: {} (digits: {}, presence: {})".format(r[6], r[4], r[5]))
         print("|{}|".format("".join("X "[numpy.array_equal(b, c) or (not pb and not pc)] for b, c, pb, pc in zip(*r_short))))
 
     def do_batch():
-        
         sess.run(train_step,
                  feed_dict={x: batch_xs, y_: batch_ys})
         if batch_idx % report_steps == 0:
             do_report()
 
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.90) ## gpu_memory_fraction = 0.96
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.90) ## OOM:0.6x
     with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
         sess.run(init)
         if initial_weights is not None:
             sess.run(assign_ops)
 
-        test_xs, test_ys = unzip(list(read_data("test/*.png"))[:50])
+        test_xs, test_ys = unzip(list(read_data("test/*.png"))[:batch_size])
 
         try:
             last_batch_idx = 0
@@ -242,13 +232,13 @@ def train(learn_rate, report_steps, batch_size, initial_weights=None):
                     if last_batch_idx != batch_idx:
                         time_for_batches = ( 60 * (last_batch_time - batch_time) / (last_batch_idx - batch_idx))
                         print("time for 60 batches {}".format(time_for_batches))
-                        print("now time: ", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                        print("now: ", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
                         last_batch_idx = batch_idx
                         last_batch_time = batch_time
 
         except KeyboardInterrupt:
             last_weights = [p.eval() for p in params]
-            numpy.savez("weights.npz", *last_weights)
+            numpy.savez("CPUweights.npz", *last_weights)
             return last_weights
         
         
@@ -262,9 +252,9 @@ if __name__ == "__main__":
                                                 key=lambda s: int(s[4:]))]
     else:
         initial_weights = None
-
+    
     train(learn_rate=0.001,
           report_steps=20,
-          batch_size=10,
+          batch_size=50,
           initial_weights=initial_weights)
     print("Train end! ", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
